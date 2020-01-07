aef0382dcab59206c0ea3dccc4cc285b4099c7af
Eswar88 <31931890+Eswar88@users.noreply.github.com>
2020-01-07 22:03:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pickle', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(np.expm1(prediction[0]), 2)
+
+    return render_template('index.html', prediction_text='Recommended price for the listing $ {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
